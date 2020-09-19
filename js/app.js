6fbdfab2e98cc528a3a82318ca4240fc366eb3e2
gertjanvdt <64344880+gertjanvdt@users.noreply.github.com>
2020-09-19 15:52:21 +0800

    style updates

--- js/app.js
+++ js/app.js
@@ -21,8 +21,19 @@ showListView.addEventListener("click", (e) => {
   slider.style.right = "0";
 });
 
+// SCROLL SLOWLY
+document.querySelectorAll('a[href^="#"]').forEach((anchor) => {
+  anchor.addEventListener("click", function (e) {
+    e.preventDefault();
+
+    document.querySelector(this.getAttribute("href")).scrollIntoView({
+      behavior: "smooth",
+    });
+  });
+});
+
 // RECOMMENDATION SLIDER
-var slideIndex = 1;
+let slideIndex = 1;
 showSlides(slideIndex);
 
 function plusSlides(n) {
@@ -34,9 +45,9 @@ function currentSlide(n) {
 }
 
 function showSlides(n) {
-  var i;
-  var slides = document.getElementsByClassName("mySlides");
-  var dots = document.getElementsByClassName("dot");
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  let dots = document.getElementsByClassName("dot");
   if (n > slides.length) {
     slideIndex = 1;
   }
